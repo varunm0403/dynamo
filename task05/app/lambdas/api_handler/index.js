06386bdeed9d373a731433c18681c479c9ffe988
varun_mishra1 <varun_mishra1@epam.com>
2025-03-09 00:24:36 +0530

    done

---
+++ task05/app/lambdas/api_handler/index.js
@@ -0,0 +1,39 @@
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { v4: uuidv4 } = require("uuid");
+
+const dynamoDb = new DynamoDBClient({ region: process.env.AWS_REGION });
+
+exports.handler = async (event) => {
+    try {
+        const requestBody = JSON.parse(event.body);
+        
+        if (!requestBody.principalId || !requestBody.content) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Missing required fields: principalId or content" })
+            };
+        }
+
+        const newItem = {
+            id: { S: uuidv4() },
+            principalId: { N: requestBody.principalId.toString() },
+            createdAt: { S: new Date().toISOString() },
+            body: { S: JSON.stringify(requestBody.content) }
+        };
+
+        await dynamoDb.send(new PutItemCommand({
+            TableName: process.env.DYNAMODB_TABLE,
+            Item: newItem
+        }));
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({ event: newItem })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Internal Server Error", details: error.message })
+        };
+    }
+};
